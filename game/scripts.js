901475626115887439e98078bbadfdc120744b61
Raul Guimaraes <raul.metalurgica134@gmail.com>
2021-08-25 14:35:04 -0300

    game working

--- game/scripts.js
+++ game/scripts.js
@@ -1,274 +1,366 @@
+/******Constants and DOM references******/
+
 let boardPositions
+let container = document.getElementById("container")
+let game = document.getElementById("game")
+let messageShow = document.getElementById("checkWinBox")
+let restartButton = document.getElementById("restart")
+let removeGame = document.getElementById("removeGame")
+let englishBoardButton = document.getElementById("englishBoard")
+let frenchBoardButton = document.getElementById("frenchBoard")
+let diamondBoardButton = document.getElementById("diamondBoard")
+let asymmetricBoardButton = document.getElementById("asymmetricBoard")
+let allRows = document.getElementsByClassName("boardRow")
+let typeGame = document.getElementById("typeGame")
 
-
-
-
-
-function startGame() {
-    createGame();
-    startEvents();
-  };
-
-//Create Game
-function createGame() {
-    let boardContainer = document.getElementById("game")
-    let rowDiv
-    let piecePosition
-    let pieceContainerDiv
-    
-    let board3 = [
-    
-      [null, null, "fill",  "fill", "fill", null,  null], 
-      [null, null, "fill",  "fill", "fill", null,  null], 
-      ["fill","fill","fill","fill","fill","fill","fill"],
-      ["fill","fill","fill",  0 , "fill","fill", "fill"], 
-      ["fill","fill","fill","fill","fill","fill","fill"], 
-      [null, null, "fill", "fill", "fill", null,   null], 
-      [null, null, "fill", "fill", "fill", null,   null]
- 
-]
-
-
-let board2 = [
-    
-  [null,null, 0,  "fill", "fill", null,  null], 
-  [null, "fill", "fill",  "fill", "fill","fill",  null], 
+const englishBoard = [
+  [null, null, "fill",  "fill", "fill", null,  null], 
+  [null, null, "fill",  "fill", "fill", null,  null], 
   ["fill","fill","fill","fill","fill","fill","fill"],
-  ["fill","fill","fill","fill", "fill","fill", "fill"], 
+  ["fill","fill","fill",  0 , "fill","fill", "fill"], 
   ["fill","fill","fill","fill","fill","fill","fill"], 
-  [null, "fill", "fill", "fill", "fill","fill",null], 
+  [null, null, "fill", "fill", "fill", null,   null], 
   [null, null, "fill", "fill", "fill", null,   null]
-
 ]
 
 
+const frenchBoard = [
+  
+  [null,    null,   0,  "fill",   "fill", null,  null], 
+  [null, "fill", "fill",  "fill", "fill","fill", null], 
+  ["fill","fill","fill","fill","fill","fill",  "fill"],
+  ["fill","fill","fill","fill", "fill","fill", "fill"], 
+  ["fill","fill","fill","fill","fill","fill",  "fill"], 
+  [null, "fill", "fill", "fill", "fill","fill",  null], 
+  [null, null, "fill", "fill", "fill",    null,  null]
 
-let board = [
-  [null,null,null, null, "fill", null,null, null,null], 
-  [null,null,null, "fill", "fill", "fill",null, null,null], 
-  [null,null,"fill", "fill", "fill", "fill","fill", null,null], 
-  [null,"fill","fill", "fill", "fill", "fill","fill", "fill",null], 
+]
+
+const diamondBoard = [
+
+  [null,  null,  null, null,   "fill", null,     null, null,  null], 
+  [null,  null,  null, "fill", "fill", "fill",  null,  null,  null], 
+  [null,null,"fill", "fill", "fill", "fill","fill",   null,   null], 
+  [null,"fill","fill", "fill", "fill", "fill","fill", "fill", null], 
   ["fill","fill","fill", "fill", 0 , "fill","fill", "fill", "fill"], 
-  [null,"fill","fill", "fill", "fill", "fill","fill", "fill",null],
-  [null,null,"fill", "fill", "fill", "fill","fill", null,null],
-  [null,null,null, "fill", "fill", "fill",null, null,null],
-  [null,null,null, null, "fill", null,null, null,null]
+  [null,"fill","fill", "fill", "fill", "fill","fill", "fill", null],
+  [null,null,"fill", "fill", "fill", "fill","fill",   null,   null],
+  [null,  null,  null, "fill", "fill", "fill",  null,  null,  null],
+  [null,  null,  null, null,   "fill", null,     null, null,  null]
+
+]
+
+const asymmetricBoard = [
+
+  [null,  null,  "fill", "fill",   "fill", null, null, null], 
+  [null,  null,  "fill", "fill",   "fill", null, null, null], 
+  [null,  null,  "fill", "fill",   "fill", null, null, null], 
+  ["fill","fill","fill", "fill", "fill","fill","fill", "fill"], 
+  ["fill","fill","fill", 0, "fill" , "fill","fill", "fill"], 
+  ["fill","fill","fill", "fill", "fill","fill","fill", "fill"],
+  [null,  null,  "fill", "fill",   "fill", null, null, null],
+  [null,  null,  "fill", "fill",   "fill", null, null, null]
+
 ]
 
 
+//startGame(englishBoard)
 
 
-    for (let i = 0; i < board.length; i++) {
-        rowDiv = document.createElement("div")
-        rowDiv.className = "boardRow"
+/**********BUTTONS*********/
 
-        
-        for (let j = 0; j < board[i].length; j++){
-            piecePosition = board[i][j];
-            pieceContainerDiv = document.createElement("div")
-            pieceContainerDiv.className = "pieceContainer"
-            
-            if (piecePosition === null) {
-
-                pieceContainerDiv.appendChild(createPiece("nullPiece", i, j))
-            }
-
-            else if (piecePosition === "fill") {
-                pieceContainerDiv.appendChild(createPiece("occupiedPiece", i, j))
-            }
-            else {
-                pieceContainerDiv.appendChild(createPiece("emptyPiece", i, j))
-            }
-
-            
-            rowDiv.appendChild(pieceContainerDiv)
-        }
-
-        
-        boardContainer.appendChild(rowDiv)
-    }
-
-    boardPositions = document.getElementsByClassName("piece")
-    let count = countPieces()
-    messageDiv.innerHTML = count + " Pieces"
-}
-
-function startEvents() {
-    let pieceDiv 
-    let classOfPiece
-
-    for (let i = 0; i < boardPositions.length; i++ ) {
-        boardPositions[i].onclick = function() {
-            classOfPiece = boardPositions[i].className 
-
-            if (classOfPiece.indexOf("occupiedPiece") >=0 ) {
-                clearSelections()
-                
-                if(classOfPiece.indexOf("selected") < 0) {
-                  boardPositions[i].className = classOfPiece + " selected"
-                    
-                }
-            }
-            else if (classOfPiece.indexOf("emptyPiece") >=0 && classOfPiece.indexOf("validDest") >= 0)  {
-                makeJump(boardPositions[i])
-                clearSelections()
-            }         
-        }
-
-    }
+//Restart Button
+restartButton.onclick = function() {
+  clearAllSelections()
+  resetGame ()
+  let numberPieces = document.getElementsByClassName("occupiedPosition")
+  messageShow.innerHTML = `${numberPieces.length} Peças` 
+  
 }
 
 
-function createPiece (type, row, col) {
+
+
+// Change Boards
+englishBoardButton.onclick = function() {
+    if (typeGame.innerHTML === "Inglês"){
+     return
+  }
+  removeTheGame()
+  createGame(englishBoard)
+  typeGame.innerHTML = "Inglês"
+  startEvents()
+}
+
+
+frenchBoardButton.onclick = function() {
+  if (typeGame.innerHTML === "Francês"){
+    return
+  }
+  removeTheGame()
+  createGame(frenchBoard)
+  typeGame.innerHTML = "Francês"
+  startEvents()
+}
+
+diamondBoardButton.onclick = function() {
+  if (typeGame.innerHTML === "Diamante"){
+    return
+  }
+  removeTheGame()
+  createGame(diamondBoard)
+  typeGame.innerHTML = "Diamante"
+  startEvents()
+}
+
+asymmetricBoardButton.onclick = function() {
+  if (typeGame.innerHTML === "Assimétrico"){
+    return
+  }
+  removeTheGame()
+  createGame(asymmetricBoard)
+  typeGame.innerHTML = "Assimétrico"
+  startEvents()
+}
+
+
+
+// Function to create and start a game
+function startGame(board) {
+  createGame(board);
+
+  startEvents()
+}
+
+
+
+//Create the Game
+function createGame(board) {
+
+
+  //Create rows in the game
+  for (let i = 0; i < board.length; i++) {
+    let rows = document.createElement("div")
+    rows.className = "boardRow"
+
+
+    //Create div that contains each piece and append the piece
+    for (let j = 0; j < board[i].length; j++) {
+      let piecePosition = board[i][j]
+      let piecePositionDiv = document.createElement("div")
+      piecePositionDiv.className = "pieceContainer"
+
+
+      if (piecePosition === null) {
+        piecePositionDiv.appendChild( createPiece("nullPiece", i, j) )
+      }
+      else if (piecePosition === "fill") {
+        piecePositionDiv.appendChild( createPiece("occupiedPosition", i, j) )
+      }
+      else {
+        piecePositionDiv.appendChild( createPiece("emptyPiece zeroPosition", i, j) )
+      }
+      
+      rows.appendChild(piecePositionDiv)
+    }
+    // Insert rows in game
+    game.appendChild(rows)
+  }
+
+  boardPositions = document.getElementsByClassName("piece")
+  let count = countPieces()
+    messageShow.innerHTML = `${count} Peças`
+}
+
+
+// Create the pieces
+function createPiece (type, i, j) {
   let div = document.createElement("div");
-  div.className = type + " piece";
-  div["data-linhas"] = row
-  div["data-colunas"] = col
+  div.className = `${type} piece`;
+  div["data-row"] = i
+  div["data-column"] = j
 
   return div;
-};
+}
 
 
-
-
-
-
-
-
-
-
-
-function checkPossibleJump(startDiv, endDiv) { //MODIFICAR
-
-    return (
-
-    // Condicionais 
-
-    //Checar se a posição final está vazia 
-      (endDiv.className.indexOf("emptyPiece") > -1) || 
-        // Checar se a posição final e inicial estão na mesma linha e se estão na mesma coluna 
-      ((startDiv["data-linhas"] === endDiv["data-linhas"] || startDiv["data-colunas"] === endDiv["data-colunas"])) ||
-
-
-    //Checar se a distancia entre a posição final e inicial é de duas casas
-      (Math.abs(startDiv["data-linhas"] - endDiv["data-linhas"]) === 2 && Math.abs(startDiv["data-colunas"] - endDiv["data-colunas"]) === 2) ||
-      (pieceInBetween(startDiv, endDiv).className.indexOf("occupiedPiece") < 0)
-    );
-};
-
-
-  
-function makeJump (destDiv) {
-      let selectedDiv
-
-      for (let i = 0; i < boardPositions.length; i++) {
-
-          
-          if (boardPositions[i].className.indexOf("selected") > 0) {
-              selectedDiv = boardPositions[i]
-              break;
-             
-          }
-        }
-
-   
-     selectedDiv.className = selectedDiv.className.replace("selected", "").replace("occupiedPiece", "emptyPiece")
-
-
-    
-     destDiv.className = destDiv.className.replace("validDest", "").replace("emptyPiece", "occupiedPiece")
-
-
-    
-     let inBetweenPiece = pieceInBetween(selectedDiv, destDiv)
-
-     inBetweenPiece.className = inBetweenPiece.className.replace("occupiedPiece", "emptyPiece")
-
-
-     let count = countPieces()
-     
- 
-     messageDiv.innerHTML = count + " Pieces"
-    
-     
-    
-     
-    
-    
-    }
-
-
-    function clearSelections(){
-    
-      let pieceDiv; 
-      for (let i = 0; i < boardPositions.length; i++) {
-        pieceDiv = boardPositions[i];
-        pieceDiv.className = pieceDiv.className.replace("selected", "").replace("validDest", "");
+// Count the Pieces
+function countPieces () {
+   let count = 0
+   for (let i = 0; i < boardPositions.length; i++) {
+     if ( boardPositions[i].className.indexOf("occupiedPosition") > -1){
+          count ++
       }
     }
-  
+    return count
+}
 
 
+// Onclick events 
+function startEvents() {
+  for (let i = 0; i < boardPositions.length; i++ ) {
 
+    boardPositions[i].onclick = function() {
+      let classOfSelectedPosition = boardPositions[i].className
 
-    function checkForVictory(){
+      if (classOfSelectedPosition.indexOf("occupiedPosition") > -1 ) {
+        clearAllSelections()
         
-        let pieceDiv 
-        
-        
-        for (let i = 0; i < boardPositions.length; i++) {
-            
-            for (let j = 0; j < boardPositions.length; j++ ) {
-              pieceDiv = boardPositions[i]
-              
-              
-
-              if (pieceDiv.className.indexOf('occupiedPiece') > -1 && checkPossibleJump(pieceDiv, boardPositions[j])) {
-                return true;
-              }
-            }
-          }
-          return false;
-        }
-
-
-
-   
-    function countPieces () {
-        let pieceDiv 
-        let count = 0 
-
-        for (let i = 0; i < boardPositions.length; i++) {
-            pieceDiv = boardPositions[i] 
-
-            if (pieceDiv.className.indexOf("occupiedPiece") > -1){
-                count ++
-            }
-        }
-
-        return count
-
-    }
-
-
-    function pieceInBetween (pieceA, pieceB) {
-          
-        if (pieceA["data-linhas"] === pieceB["data-linhas"]) {
-          row = pieceA["data-linhas"]
-          
-          col = pieceA["data-colunas"] > pieceB["data-colunas"] ? pieceA["data-colunas"] - 1 : pieceB["data-colunas"] - 1
-        } else {
-          col = pieceA["data-colunas"]
-          row = pieceA["data-linhas"] > pieceB["data-linhas"] ? pieceA["data-linhas"] - 1: pieceB["data-linhas"] - 1
-        }
-        for (let i = 0; i < boardPositions.length; i++) {
-          inBetweenPiece = boardPositions[i]
-          if (inBetweenPiece["data-linhas"] === row && inBetweenPiece["data-colunas"] === col) {
-            return inBetweenPiece
-          }
+        if(classOfSelectedPosition.indexOf("selected") < 0) {
+          boardPositions[i].classList.add("selected")
+          highlightValidJumps(boardPositions[i]);
         }
       }
+      else if (classOfSelectedPosition.indexOf("emptyPiece") > -1 &&
+               classOfSelectedPosition.indexOf("validJump") > -1)  {
+               executeJump(boardPositions[i])
+               clearAllSelections()
+        }         
+     }
+  }
+}
+
+//Clear borders and different color
+function clearAllSelections() {
+
+  for (let i = 0; i < boardPositions.length; i++) {
+    boardPositions[i].classList.remove("selected", "validJump")
+  }
+}
 
 
-          
-      startGame()
+function highlightValidJumps(startPosition) {
+
+  for (let i = 0; i < boardPositions.length; i++ ) {
+    if (jumpIsValid(startPosition, boardPositions[i])) {
+      boardPositions[i].classList.add("validJump") 
+
+    }
+  }
+}
+
+// Check if the jump is valid
+function jumpIsValid(startPosition, finalPosition) {
+
+  return !((finalPosition.className.indexOf("emptyPiece") < 0) || //Check if Empty
+
+        //Check if in same column or row
+        (!(startPosition["data-row"] === finalPosition["data-row"] || 
+        startPosition["data-column"] === finalPosition["data-column"])) ||
+
+        //Check if distance between start and final position is 2
+        (Math.abs(startPosition["data-row"] - finalPosition["data-row"]) !== 2 && 
+        Math.abs(startPosition["data-column"] - finalPosition["data-column"]) !== 2) ||
+
+       //Check if there is a piece in between
+        (getPieceInMiddle(startPosition, finalPosition).className.indexOf("occupiedPosition") < 0)
+    )
+}
+
+
+//Execute the Jump
+function executeJump (finalPosition) {
+  let selectedPiece
+  for (let i = 0; i < boardPositions.length; i++) {
+    //Find the selected Piece
+    if (boardPositions[i].className.indexOf("selected") > -1) {
+      selectedPiece = boardPositions[i];
+      break;
+    }
+  }
+
+
+ //Remove the piece at start position
+  selectedPiece.classList.remove("selected", "occupiedPosition")
+  selectedPiece.classList.add("emptyPiece")
+
+
+  //Add piece to final position
+  finalPosition.classList.remove("validJump", "emptyPiece")
+  finalPosition.classList.add("occupiedPosition")
+
+ //Remove piece in between
+   let pieceInMiddle = getPieceInMiddle(selectedPiece, finalPosition)
+
+   pieceInMiddle.classList.remove("occupiedPosition")
+   pieceInMiddle.classList.add("emptyPiece")
+
+  //Count Pieces
+   let count = countPieces()
+   messageShow.innerHTML = `${count} Peças`
+    
+  //Victory
+  if (count === 1) {
+    messageShow.innerHTML = "Você Venceu!!!"
+  }
+
+  //Defeat
+  else if (!checkIfCanWin()) {
+       messageShow.innerHTML = "Você Perdeu..."
+  }
+}
+
+//Check if It's possible to win
+function checkIfCanWin() {
+  
+  for (let i = 0; i < boardPositions.length; i++) {
+    for (let j = 0; j < boardPositions.length; j++ ) {
+      if (boardPositions[i].className.indexOf("occupiedPosition") > -1 && jumpIsValid(boardPositions[i], boardPositions[j])) {
+        return true;
+      }
+    }
+  }
+  return false;
+}
+
+//Piece between start and final positions
+function getPieceInMiddle (startPosition, finalPosition) {
+  let rowMiddle 
+  let colMiddle
+  let pieceInMiddle
+  
+  if (startPosition["data-row"] === finalPosition["data-row"]) {
+    rowMiddle = startPosition["data-row"]
+    colMiddle = (startPosition["data-column"] > finalPosition["data-column"] ? 
+    startPosition["data-column"] - 1 : finalPosition["data-column"] - 1)
+  } 
+  else {
+    colMiddle = startPosition["data-column"];
+    rowMiddle = (startPosition["data-row"] > finalPosition["data-row"] ? 
+    startPosition["data-row"] - 1 : finalPosition["data-row"] - 1)
+  }
+
+  for (let i = 0; i < boardPositions.length; i++) {
+    pieceInMiddle = boardPositions[i];
+    if (pieceInMiddle["data-row"] === rowMiddle && 
+        pieceInMiddle["data-column"] === colMiddle) {
+      return pieceInMiddle;
+    }
+  }
+}
+
+
+// Function used to restart the game
+function resetGame () {
+  for (let i = 0; i < boardPositions.length; i++ ) {
+    if (boardPositions[i].className.indexOf("emptyPiece") > -1 &&
+        boardPositions[i].className.indexOf("zeroPosition") < 0) {
+          boardPositions[i].classList.remove("emptyPiece")
+          boardPositions[i].classList.add("occupiedPosition")
+    }
+    else if (boardPositions[i].className.indexOf("zeroPosition") > -1) {
+       boardPositions[i].classList.remove("occupiedPosition")
+       boardPositions[i].classList.add("emptyPiece")
+    }         
+  }    
+}
+
+
+// Function used to change gameboards
+function removeTheGame () {
+  
+  let m = allRows.length  
+  for (let i = 0; i < m;  i++ ){
+    game.removeChild(allRows[0])
+  }
+}
